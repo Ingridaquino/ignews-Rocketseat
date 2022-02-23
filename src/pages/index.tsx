7e8c309a65be5df66a88b187dae642927af9372f
Ingrid Aquino <ingridaquinosoares@live.com>
2022-02-23 18:38:40 -0300

    :construction: Add env.local with stripe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,20 @@
+import { GetServerSideProps } from 'next';
+
 import Head from 'next/head';
+
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({ product }) {
   return (
     <>
       <Head>
@@ -16,7 +28,7 @@ export default function Home() {
 
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
           <SubscribeButton />
         </section>
@@ -24,4 +36,22 @@ export default function Home() {
       </main>  
     </>
   )
+}
+
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('price_1KWPkuEonhYPpHxwF5UVyohG', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: price.unit_amount / 100, 
+  };
+
+  return {
+    props:{
+      product,
+    }
+  }
 }
